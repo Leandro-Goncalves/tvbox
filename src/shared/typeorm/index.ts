ccf0eef05cc52634623528b5b86a45c4eeb8034d
Leandro Gonçalves <leandrogoncalvespr@hotmail.com>
2023-06-30 20:59:37 -0300

    fix: aa

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -2,6 +2,7 @@ import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
 export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
+  console.log("defaultOptions", defaultOptions);
   return createConnection(
     Object.assign(defaultOptions, {
       database:
